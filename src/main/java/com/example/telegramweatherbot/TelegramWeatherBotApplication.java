28b3012b773133fd2b15dfc8e7e0e22382039528
andriimanzar <andriimanzar@gmail.com>
2022-12-31 12:08:54 +0200

    Initial commit

---
+++ src/main/java/com/example/telegramweatherbot/TelegramWeatherBotApplication.java
@@ -0,0 +1,16 @@
+package com.example.telegramweatherbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Program entry point class.
+ */
+@SpringBootApplication
+public class TelegramWeatherBotApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TelegramWeatherBotApplication.class, args);
+  }
+
+}
