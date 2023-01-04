f7d310a7b8c10540ad0eb7b259f9ad1c1b25ca43
Andrii Manzar <81434874+andriimanzar@users.noreply.github.com>
2023-01-04 17:42:22 +0200

    Implement dispatcher handler and abstract handlers (#12)
    * Remove previous workflow file with Maven Package
    
    * Add spring-boot-starter-web dependency
    
    * Add entity representing user request
    
    * Add implementation of dispatcher handler and abstract handlers

---
+++ src/main/java/com/example/telegramweatherbot/handler/AbstractUserRequestHandler.java
@@ -0,0 +1,14 @@
+package com.example.telegramweatherbot.handler;
+
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+/**
+ * Abstract handler class, that contains some general methods for user request handlers.
+ */
+public abstract class AbstractUserRequestHandler implements UserRequestHandler {
+
+  public boolean isCommand(Update update, String command) {
+    return update.hasMessage() && update.getMessage().isCommand()
+        && update.getMessage().getText().equals(command);
+  }
+}
