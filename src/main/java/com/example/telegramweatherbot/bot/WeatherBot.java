cbac5de5918ef3a103f8a66e9f511ceaa591b5c9
andriimanzar <andriimanzar@gmail.com>
2023-01-04 15:15:29 +0200

    Add bot class configuration

---
+++ src/main/java/com/example/telegramweatherbot/bot/WeatherBot.java
@@ -0,0 +1,54 @@
+package com.example.telegramweatherbot.bot;
+
+import com.example.telegramweatherbot.exception.BotRegistrationException;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+/**
+ * Class, that represents bot. Contains methods to configure register and configure bot.
+ */
+@Component
+@RequiredArgsConstructor
+public class WeatherBot extends TelegramLongPollingBot {
+
+  @Value("${telegram-bot.token}")
+  private String botToken;
+  @Value("${telegram-bot.username}")
+  private String botUsername;
+
+  /**
+   * Method, that will be called after creation bean for this class.
+   *
+   * @throws BotRegistrationException in case bot registration was failed.
+   */
+  @PostConstruct
+  public void init() {
+    try {
+      TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+      telegramBotsApi.registerBot(this);
+    } catch (TelegramApiException e) {
+      throw new BotRegistrationException("Cannot register bot", e);
+    }
+  }
+
+  @Override
+  public String getBotToken() {
+    return botToken;
+  }
+
+  @Override
+  public void onUpdateReceived(Update update) {
+  }
+
+  @Override
+  public String getBotUsername() {
+    return botUsername;
+  }
+}
