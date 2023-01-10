96507a3a89c0b67cb9948acd2b2cb8116233d101
andriimanzar <manzar20011@gmail.com>
2023-01-10 16:06:34 +0200

    Add tests for MessageSendingService class

---
+++ src/test/java/com/manzar/telegramweatherbot/service/MessageSendingServiceTest.java
@@ -0,0 +1,46 @@
+package com.manzar.telegramweatherbot.service;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import com.manzar.telegramweatherbot.bot.WeatherBot;
+import com.manzar.telegramweatherbot.exception.MessageSendingException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+
+@ExtendWith(MockitoExtension.class)
+class MessageSendingServiceTest {
+
+  @Mock
+  private WeatherBot weatherBot;
+  @InjectMocks
+  private MessageSendingService messageSendingService;
+
+  @Test
+  void sendMessageCallsBotExecuteMethod() throws TelegramApiException {
+
+    messageSendingService.sendMessage(1L, "test", new ReplyKeyboardMarkup());
+
+    verify(weatherBot,
+        times(1)).execute(any(SendMessage.class));
+  }
+
+  @Test
+  void sendMessageThrowsAnExceptionIfMessageIsNotSent() throws TelegramApiException {
+
+    doThrow(TelegramApiException.class).when(weatherBot).execute(any(SendMessage.class));
+
+    assertThrows(MessageSendingException.class, () ->
+        messageSendingService.sendMessage(1L, "test", new ReplyKeyboardMarkup()));
+  }
+}
