75366cfd3bd8f754bceff6b7958f5d3d9d4b08de
andriimanzar <manzar20011@gmail.com>
2023-02-05 15:59:28 +0200

    Update tests

--- src/test/java/com/manzar/telegramweatherbot/util/CityNameValidatorTest.java
+++ src/test/java/com/manzar/telegramweatherbot/util/CityNameValidatorTest.java
@@ -3,14 +3,29 @@ package com.manzar.telegramweatherbot.util;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import lombok.NonNull;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.client.DefaultResponseErrorHandler;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class CityNameValidatorTest {
-  @Autowired
-  private CityNameValidator cityNameValidator;
+
+  private static CityNameValidator cityNameValidator;
+
+  @BeforeAll
+  static void beforeAll() {
+    RestTemplate restTemplate = buildRestTemplateWithoutErrorHandling();
+    cityNameValidator = new CityNameValidator(restTemplate);
+    ReflectionTestUtils.setField(cityNameValidator, "weatherMapApiKey",
+        System.getenv().get("API_OPEN_WEATHER_MAP_KEY"));
+  }
 
   @Test
   void enteredCityExistsReturnFalseIfCityNameIsIncorrect() {
@@ -20,5 +35,15 @@ class CityNameValidatorTest {
   @Test
   void enteredCityExistsReturnTrueIfCityNameIsCorrect() {
     assertTrue(cityNameValidator.enteredCityExists("London"));
+  }
+
+  private static RestTemplate buildRestTemplateWithoutErrorHandling() {
+    return new RestTemplateBuilder()
+        .errorHandler(new DefaultResponseErrorHandler() {
+          @Override
+          public void handleError(@NonNull ClientHttpResponse response) {
+          }
+        })
+        .build();
   }
 }
