53b21579aa7c08c82356a8d128f393f4afc2e4c8
andriimanzar <manzar20011@gmail.com>
2023-02-05 11:55:12 +0200

    Update RestTemplate bean definition

--- src/main/java/com/manzar/telegramweatherbot/config/AppConfig.java
+++ src/main/java/com/manzar/telegramweatherbot/config/AppConfig.java
@@ -1,15 +1,30 @@
 package com.manzar.telegramweatherbot.config;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * Configuration class. Contains application bean definitions.
+ */
 @Configuration
 public class AppConfig {
 
 
+  /**
+   * Creates RestTemplate bean without error handler.
+   */
   @Bean
-  public RestTemplate restTemplate() {
-    return new RestTemplate();
+  public RestTemplate getRestTemplate() {
+    return new RestTemplateBuilder()
+        .errorHandler(new DefaultResponseErrorHandler() {
+          @Override
+          public void handleError(ClientHttpResponse response) {
+          }
+        })
+        .build();
   }
 }
