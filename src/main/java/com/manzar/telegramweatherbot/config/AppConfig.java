87067786d7eff8a7c9216f06bba8d0eb551516d2
Andrii Manzar <81434874+andriimanzar@users.noreply.github.com>
2023-02-06 17:01:43 +0200

    Implement WeatherService (#28)
    * Add OpenWeatherMapClient bean to configuration class
    
    * Add implementation for WeatherService

--- src/main/java/com/manzar/telegramweatherbot/config/AppConfig.java
+++ src/main/java/com/manzar/telegramweatherbot/config/AppConfig.java
@@ -1,6 +1,8 @@
 package com.manzar.telegramweatherbot.config;
 
+import com.github.prominence.openweathermap.api.OpenWeatherMapClient;
 import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +15,14 @@ import org.springframework.web.client.RestTemplate;
  */
 @Configuration
 public class AppConfig {
+
+  @Value("${api.openweathermap.key}")
+  private String weatherMapApiKey;
+
+  @Bean
+  public OpenWeatherMapClient openWeatherMapClient() {
+    return new OpenWeatherMapClient(weatherMapApiKey);
+  }
 
 
   /**
