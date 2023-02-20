308f53d6e569c31c4f065388372c29e84c7646cd
andriimanzar <manzar20011@gmail.com>
2023-02-20 20:20:56 +0200

    Add javadoc

--- src/main/java/com/manzar/telegramweatherbot/model/ConversationState.java
+++ src/main/java/com/manzar/telegramweatherbot/model/ConversationState.java
@@ -1,5 +1,8 @@
 package com.manzar.telegramweatherbot.model;
 
+/**
+ * Represents conversation state between bot and user.
+ */
 public enum ConversationState {
   CONVERSATION_STARTED, WAITING_FOR_CITY, WAITING_FOR_DATE
 }
--- src/main/java/com/manzar/telegramweatherbot/model/UserSession.java
+++ src/main/java/com/manzar/telegramweatherbot/model/UserSession.java
@@ -10,6 +10,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * Represents user session.
+ */
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(of = "telegramId")
