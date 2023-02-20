25cfb4c5338417776b491367169ad904371eee62
Andrii Manzar <81434874+andriimanzar@users.noreply.github.com>
2023-02-20 20:21:48 +0200

    Add UserSession entity (#37)
    * Add UserSession entity
    
    * Add javadoc

---
+++ src/main/java/com/manzar/telegramweatherbot/model/UserSession.java
@@ -0,0 +1,33 @@
+package com.manzar.telegramweatherbot.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Represents user session.
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "telegramId")
+@Getter
+@Setter
+@Entity
+@Table(name = "user_session")
+public class UserSession {
+
+  @Id
+  @Column(name = "telegram_id")
+  private Long telegramId;
+  @Column(name = "city")
+  private String city;
+  @Column(name = "conversation_state")
+  private ConversationState conversationState;
+
+}
