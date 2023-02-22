4c6df711154522d95162f56ba6124ae1988c8ba6
andriimanzar <manzar20011@gmail.com>
2023-02-22 20:55:56 +0200

    Update migration script

--- src/main/resources/db/migration/V1__ddl.sql
+++ src/main/resources/db/migration/V1__ddl.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS user_session
 (
-    telegram_id BIGINT,
-    city        VARCHAR(255) NOT NULL,
+    telegram_id        BIGINT,
+    city               VARCHAR(255),
     conversation_state VARCHAR(255) NOT NULL,
     CONSTRAINT user_session_PK PRIMARY KEY (telegram_id)
 );
