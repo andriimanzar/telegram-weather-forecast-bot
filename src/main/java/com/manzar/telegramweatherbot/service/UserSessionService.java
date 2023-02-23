2409ba122cee96dd883c23f1cfafd9465f80020c
andriimanzar <manzar20011@gmail.com>
2023-02-23 20:10:01 +0200

    Add UserSessionService implementation

---
+++ src/main/java/com/manzar/telegramweatherbot/service/UserSessionService.java
@@ -0,0 +1,35 @@
+package com.manzar.telegramweatherbot.service;
+
+import com.manzar.telegramweatherbot.model.ConversationState;
+import com.manzar.telegramweatherbot.model.UserSession;
+import com.manzar.telegramweatherbot.repository.UserSessionRepository;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserSessionService {
+
+  private final UserSessionRepository userSessionRepository;
+
+  public Optional<UserSession> getUserSession(Long telegramId) {
+    return userSessionRepository.findById(telegramId);
+  }
+
+  public void createUserSession(Long telegramId) {
+    if (getUserSession(telegramId).isEmpty()) {
+      UserSession userSessionToSave = UserSession.builder().telegramId(telegramId)
+          .conversationState(
+              ConversationState.CONVERSATION_STARTED).build();
+
+      userSessionRepository.save(userSessionToSave);
+    }
+  }
+
+  public void editUserSession(UserSession updatedSession) {
+    userSessionRepository.save(updatedSession);
+  }
+
+
+}
