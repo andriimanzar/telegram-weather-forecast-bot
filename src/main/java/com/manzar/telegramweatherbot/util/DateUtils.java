9d82ccab3504eac24e9ac0f85dd4b65c8286eb20
andriimanzar <manzar20011@gmail.com>
2023-02-28 23:30:37 +0200

    Add DateUtils class

---
+++ src/main/java/com/manzar/telegramweatherbot/util/DateUtils.java
@@ -0,0 +1,24 @@
+package com.manzar.telegramweatherbot.util;
+
+import java.time.LocalDate;
+
+/**
+ * Provides utility methods for working with dates.
+ */
+public class DateUtils {
+
+  public static boolean isValid(String date) {
+    return date.matches("^(0?[1-9]|[12][0-9]|3[01])/(0?[1-9]|1[012])$");
+  }
+
+  /**
+   * Parses a date string using the specified format string.
+   */
+  public static LocalDate parse(String date) {
+    int day = Integer.parseInt(date.split("/")[0]);
+    int month = Integer.parseInt(date.split("/")[1]);
+    return LocalDate.of(LocalDate.now().getYear(), month, day);
+  }
+
+
+}
