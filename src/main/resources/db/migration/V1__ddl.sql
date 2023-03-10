30a8676b8c75cdb22bd496fd8c0c1b38acffa73b
andriimanzar <manzar20011@gmail.com>
2023-03-10 21:14:44 +0200

    Add notifications table scheme into migration script

--- src/main/resources/db/migration/V1__ddl.sql
+++ src/main/resources/db/migration/V1__ddl.sql
@@ -5,3 +5,14 @@ CREATE TABLE IF NOT EXISTS user_session
     conversation_state VARCHAR(255) NOT NULL,
     CONSTRAINT user_session_PK PRIMARY KEY (telegram_id)
 );
+
+CREATE TABLE IF NOT EXISTS notifications
+(
+    id                BIGSERIAL,
+    telegram_id       BIGINT,
+    notification_type VARCHAR(255),
+    notification_time TIME WITH TIME ZONE,
+    CONSTRAINT notifications_PK PRIMARY KEY (id),
+    CONSTRAINT notifications_user_session_FK FOREIGN KEY (telegram_id) REFERENCES user_session (telegram_id)
+
+);
