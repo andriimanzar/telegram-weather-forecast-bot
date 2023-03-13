ba4ff4379fe5855fce5c68365e838ea0d0cd4a87
andriimanzar <manzar20011@gmail.com>
2023-03-13 21:19:47 +0200

    Add chatId column into notification table scheme

--- src/main/resources/db/migration/V1__ddl.sql
+++ src/main/resources/db/migration/V1__ddl.sql
@@ -5,3 +5,15 @@ CREATE TABLE IF NOT EXISTS user_session
     conversation_state VARCHAR(255) NOT NULL,
     CONSTRAINT user_session_PK PRIMARY KEY (telegram_id)
 );
+
+CREATE TABLE IF NOT EXISTS notifications
+(
+    id                BIGSERIAL,
+    telegram_id       BIGINT,
+    chat_id           BIGINT,
+    notification_type VARCHAR(255),
+    notification_time TIME WITHOUT TIME ZONE,
+    CONSTRAINT notifications_PK PRIMARY KEY (id),
+    CONSTRAINT notifications_user_session_FK FOREIGN KEY (telegram_id) REFERENCES user_session (telegram_id)
+
+);
