41aa65f6aeae841f9bf2dd7c957aacc4dabb4c42
andriimanzar <manzar20011@gmail.com>
2023-03-27 23:17:37 +0300

    Add ForecastFormatter localization

--- src/main/java/com/manzar/telegramweatherbot/util/ForecastFormatter.java
+++ src/main/java/com/manzar/telegramweatherbot/util/ForecastFormatter.java
@@ -1,37 +1,38 @@
 package com.manzar.telegramweatherbot.util;
 
 import com.github.prominence.openweathermap.api.model.forecast.WeatherForecast;
+import com.github.prominence.openweathermap.api.model.forecast.Wind;
+import com.manzar.telegramweatherbot.model.UserSession;
+import com.manzar.telegramweatherbot.service.LocalizationService;
+import java.time.LocalDate;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 /**
  * Provides methods for formatting weather forecast data.
  */
 @Component
+@RequiredArgsConstructor
 public class ForecastFormatter {
 
   public static final String LINE_SEPARATOR = System.lineSeparator();
+  private final LocalizationService localizationService;
 
   /**
    * Formats the weather forecast for a given city and day.
    *
+   * @param userSession           current user session
    * @param requestedDayForecasts the list of weather forecasts for the requested day.
-   * @param cityName              the name of the city for which the weather forecast is being
-   *                              formatted.
    * @return a formatted string containing the weather forecast for the specified city and day or
-   *     informs user that there is no forecast for requested day.
+   * informs user that there is no forecast for requested day.
    */
-  public String format(List<WeatherForecast> requestedDayForecasts, String cityName) {
+  public String format(UserSession userSession, List<WeatherForecast> requestedDayForecasts) {
     if (requestedDayForecasts.isEmpty()) {
-      return "🙏 Thank you for using our service! "
-          + "Our predictions for this period have ended or we were unable to request a forecast "
-          + "because we can only parse weather for the next 5 days. Please try again later.";
+      return "predictions.ended";
     }
-
     StringBuilder stringBuilder = new StringBuilder();
 
-    stringBuilder.append("Weather in ").append(cityName).append(", ");
-    stringBuilder.append(requestedDayForecasts.get(0).getForecastTime().toLocalDate());
     stringBuilder.append(LINE_SEPARATOR);
 
     for (WeatherForecast weatherForecast : requestedDayForecasts) {
@@ -39,10 +40,14 @@ public class ForecastFormatter {
       appendWeatherStateEmoji(weatherForecast, stringBuilder);
       appendTemperature(weatherForecast, stringBuilder);
       appendClouds(weatherForecast, stringBuilder);
-      appendWind(weatherForecast, stringBuilder);
+      appendWind(userSession, weatherForecast, stringBuilder);
       stringBuilder.append(LINE_SEPARATOR);
     }
-    return stringBuilder.toString();
+    LocalDate requestedDay = requestedDayForecasts.get(0).getForecastTime().toLocalDate();
+    String[] params = new String[]{userSession.getCity(), requestedDay.toString(),
+        stringBuilder.toString()};
+    return localizationService.localizeMessage(userSession, "weather.forecast",
+        params);
   }
 
   private void appendWeatherStateEmoji(WeatherForecast weatherForecast,
@@ -63,14 +68,15 @@ public class ForecastFormatter {
   }
 
   private void appendClouds(WeatherForecast weatherForecast, StringBuilder stringBuilder) {
-    stringBuilder.append("clouds - ").append(weatherForecast.getClouds().getValue())
+    stringBuilder.append("☁️- ").append(weatherForecast.getClouds().getValue())
         .append(weatherForecast.getClouds().getUnit()).append(", ");
   }
 
-  private void appendWind(WeatherForecast weatherForecast, StringBuilder stringBuilder) {
-    double windSpeed = weatherForecast.getWind().getSpeed();
-    stringBuilder.append("💨").append(String.format("%.1f", windSpeed)).append(" ")
-        .append("m/s");
+  private void appendWind(UserSession userSession, WeatherForecast weatherForecast,
+      StringBuilder stringBuilder) {
+    Wind wind = weatherForecast.getWind();
+    stringBuilder.append("💨").append(String.format("%.1f", wind.getSpeed())).append(" ")
+        .append(localizationService.localizeMessage(userSession, wind.getUnit(), null));
   }
 }
 
