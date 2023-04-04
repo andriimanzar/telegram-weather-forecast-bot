47f7b1178d8550ca4ce1545d9439faf566431a61
andriimanzar <manzar20011@gmail.com>
2023-04-04 15:03:51 +0300

    Update tests

--- src/test/java/com/manzar/telegramweatherbot/handler/NotificationTimeHandlerTest.java
+++ src/test/java/com/manzar/telegramweatherbot/handler/NotificationTimeHandlerTest.java
@@ -3,6 +3,7 @@ package com.manzar.telegramweatherbot.handler;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import com.manzar.telegramweatherbot.keyboard.StartMenuKeyboardBuilder;
 import com.manzar.telegramweatherbot.model.ConversationState;
@@ -65,6 +66,10 @@ class NotificationTimeHandlerTest {
     UserSession userSession = userRequest.getUserSession();
     String city = userSession.getCity();
     String[] params = new String[]{city, notificationTime};
+    LocalTime localTime = LocalTime.parse(notificationTime);
+
+    when(notificationService.createTomorrowNotification(userSession,
+        Optional.of(localTime))).thenReturn(true);
 
     notificationTimeHandler.handle(userRequest);
 
@@ -75,15 +80,38 @@ class NotificationTimeHandlerTest {
 
   @Test
   void handleChangesSessionStateToConversationStartedIfDateIsValid() {
+    String notificationTime = "07:00";
     UserRequest userRequest = UserRequestFactory.createRequestWithStateAndText(
-        ConversationState.WAITING_FOR_NOTIFICATION_TIME, "07:00");
+        ConversationState.WAITING_FOR_NOTIFICATION_TIME, notificationTime);
     UserSession userSession = userRequest.getUserSession();
+    LocalTime localTime = LocalTime.parse(notificationTime);
+
+    when(notificationService.createTomorrowNotification(userSession,
+        Optional.of(localTime))).thenReturn(true);
 
     notificationTimeHandler.handle(userRequest);
 
     assertEquals(ConversationState.CONVERSATION_STARTED, userSession.getConversationState());
     verify(userSessionService, times(1)).editUserSession(userSession);
   }
+
+  @Test
+  void handleSendsMessageAboutExistingTomorrowNotificationIfNotificationForGivenTimeAlreadyExists() {
+    String notificationTime = "07:00";
+    UserRequest userRequest = UserRequestFactory.createRequestWithStateAndText(
+        ConversationState.WAITING_FOR_NOTIFICATION_TIME, notificationTime);
+    UserSession userSession = userRequest.getUserSession();
+    LocalTime localTime = LocalTime.parse(notificationTime);
+
+    when(notificationService.createTomorrowNotification(userSession,
+        Optional.of(localTime))).thenReturn(false);
+
+    notificationTimeHandler.handle(userRequest);
+
+    verify(messageSendingService, times(1))
+        .sendMessage(userSession, "tomorrow.notification.with.same.time.exists");
+  }
+
 
   @Test
   void handleCreatesTomorrowNotification() {
@@ -94,7 +122,7 @@ class NotificationTimeHandlerTest {
     notificationTimeHandler.handle(userRequest);
 
     verify(notificationService, times(1)).createTomorrowNotification(userRequest.getUserSession(),
-        userRequest.getChatId(), Optional.of(notificationTime));
+        Optional.of(notificationTime));
   }
 
   private static Stream<Arguments> provideUserRequestsAndResults() {
@@ -103,11 +131,11 @@ class NotificationTimeHandlerTest {
     requestWithoutText.getUpdate().getMessage().setText(null);
     return Stream.of(
         Arguments.of(
-        UserRequestFactory.createRequestWithStateAndTestMessage(
-            ConversationState.WAITING_FOR_NOTIFICATION_TIME),
-        true),
+            UserRequestFactory.createRequestWithStateAndTestMessage(
+                ConversationState.WAITING_FOR_NOTIFICATION_TIME),
+            true),
         Arguments.of(UserRequestFactory.createRequestWithStateAndTestMessage(
-        ConversationState.CONVERSATION_STARTED), false),
+            ConversationState.CONVERSATION_STARTED), false),
         Arguments.of(requestWithoutText, false));
   }
 }
